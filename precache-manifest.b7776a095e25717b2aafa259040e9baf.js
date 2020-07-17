310ca0d3b3213fd3e90554501f2ffd1ac369d39d
Yini Shangguan <yinishangguan@Yinis-MBP.attlocal.net>
2020-07-17 16:00:43 -0700

    Updates

---
+++ precache-manifest.b7776a095e25717b2aafa259040e9baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367b02e8ff4e1b33ee7a82ae44d2350d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "20ad35c297a07059613e",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ad35c297a07059613e",
+    "url": "/robofriends/static/js/main.d20ca509.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
